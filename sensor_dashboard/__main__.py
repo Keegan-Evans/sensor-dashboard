cffad93076cbf52b22051e5669d629b27bac38d4
Keegan Evans <keegan.evans@gmail.com>
2023-10-10 22:16:44 -0700

    BOOKMARK

--- sensor_dashboard/__main__.py
+++ sensor_dashboard/__main__.py
@@ -1,5 +1,5 @@
-from connection.connection import get_queried_df
-from munge_and_plot.munge_and_plot import (
+from sensor_dashboard.connection import get_queried_df
+from  sensor_dashboard.munge_and_plot import (
     create_wind_polar_plot, munge_wind_data, create_wind_speed_plot,
     create_rainfall_plot,
 )
@@ -12,28 +12,16 @@ from dash import Dash, html, dcc
 # LOOP
 
 # read from database
-df = get_queried_df()
 # munge wind data for polar plot
 
-wind_data = munge_wind_data(df)
 
-# create polar plot for wind direction
-wind_dir_fig = create_wind_polar_plot(wind_data)
-wind_dir_fig.to_html('wind_dir_fig.html')
-
-# create scatter plot for wind speed
-wind_spd_fig = create_wind_speed_plot(df)
-wind_spd_fig.to_html('wind_spd_fig.html')
-
-# create scatter plot for rainfall
-rainfall_fig = create_rainfall_plot(df)
-rainfall_fig.to_html('rainfall_fig.html')
 
 # create and layout dash app
 app = Dash(__name__)
 app.layout = html.Div([
 
-    html.H1(children='Weather Station Data', style={'textAlign': 'center'}),
+    html.H1(id='weather_station', children='Weather Station Data', style={'textAlign': 'center'}),
+    dcc.Interval(id='interval', interval=1000 * 1),
 
     html.H2(children='Wind Direction Historic', style={'textAlign': 'left'}),
     dcc.Graph(id='wind_dir', figure=wind_dir_fig),
@@ -45,10 +33,35 @@ app.layout = html.Div([
     dcc.Graph(id='rainfall', figure=rainfall_fig),
 ])
 
+@app.callback(
+    Output('weather_station', 'children'),
+    [Input('interval', 'n_intervals')]
+)
+def update_from_database():
+    df = get_queried_df()
+
+    wind_data = munge_wind_data(df)
+
+    # create polar plot for wind direction
+    wind_dir_fig = create_wind_polar_plot(wind_data)
+    wind_dir_fig.to_html('wind_dir_fig.html')
+
+    # create scatter plot for wind speed
+    wind_spd_fig = create_wind_speed_plot(df)
+    wind_spd_fig.to_html('wind_spd_fig.html')
+
+    # create scatter plot for rainfall
+    rainfall_fig = create_rainfall_plot(df)
+    rainfall_fig.to_html('rainfall_fig.html')   
+    return [wind_dir_fig, wind_spd_fig, rainfall_fig]
+
 
 def main():
     app.run(debug=True)
     print("ran app")
+
+if __name__ == '__main__':
+    main()
 
 
 # Do I need to do a manual loop or does the dash app handle this for me?
