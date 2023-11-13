bcaba0901c7d16a82b27a65f6920b623808e97b5
Keegan Evans <keegan.evans@gmail.com>
2023-11-13 22:54:22 +0000

    IMP: implements querying by start and end dates

--- sensor_dashboard/connection/connection.py
+++ sensor_dashboard/connection/connection.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, select, desc
 from sqlalchemy.orm import joinedload
 from mqtt_data_logger.sensor_data_models import SensorMeasurement
 import pandas as pd
+from datetime import date, datetime, time
 import pytest
 
 default_fp = os.path.join("/", "home", "beta", "sensor_data.db")
@@ -10,7 +11,7 @@ testing_fp = os.path.join(".", "sensor_data.db")
 
 # Session = sessionmaker(bind=sqlite_engine)
 
-def get_queried_df(db_fp=testing_fp, number_of_observations=None, drop_zeros=True):
+def get_queried_df(db_fp=testing_fp, number_of_observations=None, drop_zeros=True, start_date=None, end_date=None):
 
     """
     db_fp: path to the database file. By default, this is the path to `sensor_dashboard/home/beta/sensor_data.db` by `connection.py`.
@@ -28,20 +29,32 @@ def get_queried_df(db_fp=testing_fp, number_of_observations=None, drop_zeros=Tru
                                         joinedload(SensorMeasurement.
                                                    measurement)).order_by(SensorMeasurement.time.desc())
 
-        match number_of_observations:
-            case None:
-                pass
-            case int():
-                query = query.limit(number_of_observations)
 
         if drop_zeros:
             query = query.where(SensorMeasurement.value > 0)
+
+        if start_date:
+            if not isinstance(start_date, date):
+                raise ValueError("Provided value({}) for `start_date` is type {}, not datetime.date.".format(start_date, type(start_date)))
+            query = query.where(SensorMeasurement.time >= start_date)
+
+        if end_date:
+            if not isinstance(end_date, date):
+                raise ValueError("Provided value({}) for `end_date` is type {}, not datetime.date.".format(end_date, type(end_date)))
+            end_date = date(end_date.year, end_date.month, end_date.day + 1)
+            query = query.where(SensorMeasurement.time < end_date) # datetime(end_date, time(23, 59)))
             
                 
         # if number_of_observations is not None:
             # print("VAlUE OTHER THAN NONE OBSERVED")
             # query = query.limit(number_of_observations)
 
+        # limit number only after all other selection takes place
+        match number_of_observations:
+            case None:
+                pass
+            case int():
+                query = query.limit(number_of_observations)
 
         queried_df = pd.read_sql_query(query, connection)
         return queried_df
--- sensor_dashboard/tests/test_connection.py
+++ sensor_dashboard/tests/test_connection.py
@@ -2,15 +2,14 @@ from sensor_dashboard.connection import get_queried_df
 import pytest
 import os
 
+from datetime import date, datetime
+
 test_filepath =os.path.join(# "sensor-dashboard", # "tests", "data",
                             "sensor_data.db")
-print(test_filepath)
-print("/workspaces/sensor-dashboard/sensor_data.db")
 
 
 def test_basic_connection_no_limit():
     qdf = get_queried_df(db_fp=test_filepath, number_of_observations=None, drop_zeros=False)
-    print(qdf['time'])
     assert qdf['time'].count() == 76026
 
 def test_basic_connection_default_cnt():
@@ -20,3 +19,27 @@ def test_basic_connection_default_cnt():
 def test_eliminate_zero_obs():
     queried_df = get_queried_df(db_fp=test_filepath, number_of_observations=None, drop_zeros=True)
     assert queried_df['time'].count() == 44754
+
+def test_raises_error_wrong_start_date_type():
+    with pytest.raises(ValueError):
+        get_queried_df(db_fp=test_filepath, start_date="not a start date")
+
+def test_subsets_by_start_date():
+    start_date = date(2023, 10, 31)
+    queried_df = get_queried_df(db_fp=test_filepath, start_date=start_date)
+    assert queried_df['time'].min() >= date(2023, 10, 31)
+
+def test_subsets_by_end_date():
+    end_date = date(2023, 11, 8)
+    queried_df = get_queried_df(db_fp=test_filepath, end_date=end_date)
+
+    print(queried_df['time'].max().date())
+    assert queried_df['time'].max() == end_date
+
+def test_subsets_by_start_and_end_date():
+    start_date = date(2023, 10, 31)
+    end_date = date(2023, 11, 8)
+    queried_df = get_queried_df(db_fp=test_filepath, start_date=start_date, end_date=end_date)
+
+    assert queried_df['time'].min() == start_date
+    assert queried_df['time'].max() == end_date
